bdbdc95d7f67fe3fe88e0c3125d5531b7ad893ea
Rohit Verma <28vrohit@gmail.com>
2025-11-12 19:08:15 +0530

    sdf

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ import random from "random";
 
 const path = "./data.json";
 
+// Configuration - Set your start and end dates here
+const CONFIG = {
+  startDate: moment("2025-11-08"), // Start date
+  endDate: moment("2025-11-11"), // End date
+  commitCount: 100, // Number of commits to create
+};
+
 const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+  const date = moment(CONFIG.startDate).add(x, "w").add(y, "d").format();
 
   const data = {
     date: date,
@@ -26,11 +28,16 @@ const makeCommits = (n) => {
   if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  let date = moment().subtract(4, "y").add(1, "d").add(x, "w").add(y, "d");
+  let date = moment(CONFIG.startDate).add(x, "w").add(y, "d");
 
-  if (date.isAfter(moment())) {
-    date = moment().subtract(1, "d"); // Set to one day before today if needed
+  // Ensure date is within the configured range
+  if (date.isAfter(CONFIG.endDate)) {
+    date = moment(CONFIG.endDate);
   }
+  if (date.isBefore(CONFIG.startDate)) {
+    date = moment(CONFIG.startDate);
+  }
+
   date = date.format();
   const data = {
     date: date,
@@ -43,4 +50,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(100);
+makeCommits(CONFIG.commitCount);
